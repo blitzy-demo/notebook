9336579a663bd58d22ae3a81cfa6aae7a13a2063
blitzy-demo <postman@blitzy.com>
2025-05-23 13:10:02 -0400

    File: packages/notebook/style/index.js

---
+++ packages/notebook/style/index.js
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+
+// Import core application styles
+import '@jupyterlab/application/style/index.js';
+
+// Import UI component styles
+import '@jupyterlab/ui-components/style/index.js';
+
+// Import local notebook styles
+import './base.css';
+import './variables.css';
+import './collaboration.css';
